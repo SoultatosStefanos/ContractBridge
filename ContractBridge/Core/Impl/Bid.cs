2500814e3f9cf152430d0da6e63b885ca7dae916
SoultatosStefanos <stefanoss1498@gmail.com>
2024-06-24 21:26:16 +0300

    Equals and formatters

--- ContractBridge/Core/Impl/Bid.cs
+++ ContractBridge/Core/Impl/Bid.cs
@@ -11,5 +11,30 @@ namespace ContractBridge.Core.Impl
         public Level Level { get; }
 
         public Denomination Denomination { get; }
+
+        private bool Equals(IBid other)
+        {
+            return Level == other.Level && Denomination == other.Denomination;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Bid)obj);
+        }
+        
+        public override string ToString()
+        {
+            return $"{nameof(Level)}: {Level}, {nameof(Denomination)}: {Denomination}";
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((int)Level * 397) ^ (int)Denomination;
+            }
+        }
     }
 }
